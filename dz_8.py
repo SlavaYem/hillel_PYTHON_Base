43306dab106f2bea5e9a6beb58098af6aec3d885
Yemelianov Vyacheslav <slavaemelanov80@gmail.com>
2022-10-03 15:20:53 +0300

    Dz_7 commit

--- dz_8.py
+++ dz_8.py
@@ -1,69 +1,69 @@
-############################################################################################################
-# my_list = ["qwe", "zxc", "ert", "tyu"]
-# new_list = []
-# for i, num in enumerate(my_list):
-#     if i % 2:
-#         new_list.append(num[::-1])
-#     else:
-#         new_list.append(num)
-# print(new_list)
-############################################################################################################
-# my_list = ["abc","dbc","zxc"]
-# new_list = []
-# for index in my_list:
-#      if "a" == index[0]:
-#          new_list.append(index)
-# print(new_list)
-############################################################################################################
-# my_list = ["abc","dbc","zxc" ," dfsf aa"]
-# new_list = []
-# for index in my_list:
-#      if "a" in index:
-#          new_list.append(index)
-# print(new_list)
-############################################################################################################
-# persons = [{"name": "John", "age": 15},
-#            {"name": "Anna", "age": 27},
-#            {"name": "Johna", "age": 30}]
-# youngest_person = []
-# ages = []
-# longest_person_name = []
-# name_lens = []
-#
-# for person_dict in persons:
-#     ages.append(person_dict["age"])
-#     name_lens.append(len(person_dict["name"]))
-# # print(name_lens, ages)
-# min_age = min(ages)
-# max_len_name = max(name_lens)
-#
-# for person_dict in persons:
-#     if person_dict["age"] == min_age:
-#         youngest_person.append(person_dict["name"])
-#     if len(person_dict["name"]) == max_len_name:
-#         longest_person_name.append(person_dict["name"])
-#
-# mean_age = sum(ages) / len(ages)
-#
-# print(youngest_person, longest_person_name, mean_age)
-############################################################################################################
-# my_dict_1 = {1: 1, 2: 2, 3: 3, 11: 100}
-# my_dict_2 = {11: 11, 2: 22}
-#
-# result_1 = list(set(my_dict_1.keys()).intersection(set(my_dict_2.keys())))
-# print(result_1)
-#
-# result_2 = list(set(my_dict_1.keys()).difference(set(my_dict_2.keys())))
-# print(result_2)
-#
-# result_3 = {key: my_dict_1[key] for key in result_2}
-# print(result_3)
-#
-# result_4 = my_dict_1.copy()
-# for key in my_dict_2:
-#     if key in result_4:
-#         result_4[key] = [result_4[key], my_dict_2[key]]
-#     else:
-#         result_4[key] = my_dict_2[key]
-# print(result_4)
-############################################################################################################
+###########################################################################################################
+my_list = ["qwe", "zxc", "ert", "tyu"]
+new_list = []
+for i, num in enumerate(my_list):
+    if i % 2:
+        new_list.append(num[::-1])
+    else:
+        new_list.append(num)
+print(new_list)
+###########################################################################################################
+my_list = ["abc","dbc","zxc"]
+new_list = []
+for index in my_list:
+     if "a" == index[0]:
+         new_list.append(index)
+print(new_list)
+###########################################################################################################
+my_list = ["abc","dbc","zxc" ," dfsf aa"]
+new_list = []
+for index in my_list:
+     if "a" in index:
+         new_list.append(index)
+print(new_list)
+###########################################################################################################
+persons = [{"name": "John", "age": 15},
+           {"name": "Anna", "age": 27},
+           {"name": "Johna", "age": 30}]
+youngest_person = []
+ages = []
+longest_person_name = []
+name_lens = []
+
+for person_dict in persons:
+    ages.append(person_dict["age"])
+    name_lens.append(len(person_dict["name"]))
+# print(name_lens, ages)
+min_age = min(ages)
+max_len_name = max(name_lens)
+
+for person_dict in persons:
+    if person_dict["age"] == min_age:
+        youngest_person.append(person_dict["name"])
+    if len(person_dict["name"]) == max_len_name:
+        longest_person_name.append(person_dict["name"])
+
+mean_age = sum(ages) / len(ages)
+
+print(youngest_person, longest_person_name, mean_age)
+###########################################################################################################
+my_dict_1 = {1: 1, 2: 2, 3: 3, 11: 100}
+my_dict_2 = {11: 11, 2: 22}
+
+result_1 = list(set(my_dict_1.keys()).intersection(set(my_dict_2.keys())))
+print(result_1)
+
+result_2 = list(set(my_dict_1.keys()).difference(set(my_dict_2.keys())))
+print(result_2)
+
+result_3 = {key: my_dict_1[key] for key in result_2}
+print(result_3)
+
+result_4 = my_dict_1.copy()
+for key in my_dict_2:
+    if key in result_4:
+        result_4[key] = [result_4[key], my_dict_2[key]]
+    else:
+        result_4[key] = my_dict_2[key]
+print(result_4)
+###########################################################################################################
--- dz_9.py
+++ dz_9.py
@@ -1,105 +1,105 @@
-# def reverse_str_in_list(my_list_in_def):
-#     for i, num in enumerate(my_list_in_def):
-#         if i % 2:
-#             new_list.append(num[::-1])
-#         else:
-#             new_list.append(num)
-#     return new_list
-#
-#
-# new_list = []
-# my_list = ["qwe", "zxc", "ert", "tyu"]
-# new_list = reverse_str_in_list(my_list)
-# print(new_list)
-###############################################################################
-# def a_first_in_str_in_list(my_list_in_def):
-#     for index in my_list_in_def:
-#         if "a" == index[0]:
-#             new_list.append(index)
-#     return new_list
-#
-#
-# my_list = ["abc", "dbc", "zxc"]
-# new_list = []
-# new_list = a_in_str_in_list(my_list)
-# print(new_list)
-###############################################################################
-# def a_in_str_in_list(my_list_in_def):
-#     for index in my_list_in_def:
-#         if "a" in index:
-#             new_list.append(index)
-#     return new_list
-#
-#
-# my_list = ["abc","dbc","zxc" ," d aa"]
-# new_list = []
-# new_list = a_in_str_in_list(my_list)
-# print(new_list)
-###############################################################################
-# def str_not_int_in_list(my_list_in_def):
-#     for index in range(len(my_list_in_def)):
-#         if type(my_list_in_def[index]) == str:
-#             my_new_list.append(my_list_in_def[index])
-#     return my_new_list
-#
-# my_new_list = []
-# my_list = [1, 2, 3, "11", "22", 33]
-# my_new_list = str_not_int_in_list(my_list)
-# print(my_new_list)
-###############################################################################
-# def symbols_alone_in_str(my_str_in_def):
-#     my_list_in_def = [i for i in my_str if my_str.count(i) == 1]
-#     return my_list_in_def
-#
-# my_str = " qweeeeee"
-# my_list = symbols_alone_in_str(my_str)
-# print(my_list)
-###############################################################################
-# def symbols_in_lists(my_str_1_in_def,my_str_2_in_def):
-#     for i in my_str_1_in_def:
-#         if i in my_str_2_in_def:
-#             result.append(i)
-#     return result
-#
-#
-#
-# my_str_1 = "qwe dfsfdsf e q "
-# my_str_2 = "qwe  e  q"
-# result = []
-# my_result = symbols_in_lists(my_str_1,my_str_2)
-# print(my_result)
-###############################################################################
-# def symbols_in_lists(my_str_1_in_def, my_str_2_in_def):
-#     for i in my_str_1_in_def:
-#         k = my_str_1_in_def.find(i) - my_str_1_in_def.rfind(i)
-#         if k == 0:
-#             if i in my_str_2_in_def and my_str_2_in_def.find(i) - my_str_2_in_def.rfind(i) == 0:
-#                 result.append(i)
-#     return result
-#
-#
-# my_str_1 = "qwe dfsfdsf e q "
-# my_str_2 = "qwe  e  q"
-# result = []
-# my_result = symbols_in_lists(my_str_1, my_str_2)
-# print(my_result)
-###############################################################################
-# import random
-# import string
-#
-#
-# def create_email(names_in_def, domains_in_def):
-#     number_random = random.randint(100, 1000)
-#     number_random_str = str(number_random)
-#     rand_string = ''.join(random.choices(string.ascii_lowercase, k=random.randint(5, 7)))
-#     my_str_1 = random.choice(names_in_def) + "." + number_random_str + "@" + rand_string + "." + random.choice(domains_in_def)
-#     return my_str_1
-#
-#
-# names = ["king", "miller", "kean"]
-# domains = ["net", "com", "ua"]
-#
-# e_mail = create_email(names, domains)
-# print(e_mail)
+def reverse_str_in_list(my_list_in_def):
+    for i, num in enumerate(my_list_in_def):
+        if i % 2:
+            new_list.append(num[::-1])
+        else:
+            new_list.append(num)
+    return new_list
+
+
+new_list = []
+my_list = ["qwe", "zxc", "ert", "tyu"]
+new_list = reverse_str_in_list(my_list)
+print(new_list)
+##############################################################################
+def a_first_in_str_in_list(my_list_in_def):
+    for index in my_list_in_def:
+        if "a" == index[0]:
+            new_list.append(index)
+    return new_list
+
+
+my_list = ["abc", "dbc", "zxc"]
+new_list = []
+new_list = a_in_str_in_list(my_list)
+print(new_list)
+##############################################################################
+def a_in_str_in_list(my_list_in_def):
+    for index in my_list_in_def:
+        if "a" in index:
+            new_list.append(index)
+    return new_list
+
+
+my_list = ["abc","dbc","zxc" ," d aa"]
+new_list = []
+new_list = a_in_str_in_list(my_list)
+print(new_list)
+##############################################################################
+def str_not_int_in_list(my_list_in_def):
+    for index in range(len(my_list_in_def)):
+        if type(my_list_in_def[index]) == str:
+            my_new_list.append(my_list_in_def[index])
+    return my_new_list
+
+my_new_list = []
+my_list = [1, 2, 3, "11", "22", 33]
+my_new_list = str_not_int_in_list(my_list)
+print(my_new_list)
+##############################################################################
+def symbols_alone_in_str(my_str_in_def):
+    my_list_in_def = [i for i in my_str if my_str.count(i) == 1]
+    return my_list_in_def
+
+my_str = " qweeeeee"
+my_list = symbols_alone_in_str(my_str)
+print(my_list)
+##############################################################################
+def symbols_in_lists(my_str_1_in_def,my_str_2_in_def):
+    for i in my_str_1_in_def:
+        if i in my_str_2_in_def:
+            result.append(i)
+    return result
+
+
+
+my_str_1 = "qwe dfsfdsf e q "
+my_str_2 = "qwe  e  q"
+result = []
+my_result = symbols_in_lists(my_str_1,my_str_2)
+print(my_result)
+##############################################################################
+def symbols_in_lists(my_str_1_in_def, my_str_2_in_def):
+    for i in my_str_1_in_def:
+        k = my_str_1_in_def.find(i) - my_str_1_in_def.rfind(i)
+        if k == 0:
+            if i in my_str_2_in_def and my_str_2_in_def.find(i) - my_str_2_in_def.rfind(i) == 0:
+                result.append(i)
+    return result
+
+
+my_str_1 = "qwe dfsfdsf e q "
+my_str_2 = "qwe  e  q"
+result = []
+my_result = symbols_in_lists(my_str_1, my_str_2)
+print(my_result)
+##############################################################################
+import random
+import string
+
+
+def create_email(names_in_def, domains_in_def):
+    number_random = random.randint(100, 1000)
+    number_random_str = str(number_random)
+    rand_string = ''.join(random.choices(string.ascii_lowercase, k=random.randint(5, 7)))
+    my_str_1 = random.choice(names_in_def) + "." + number_random_str + "@" + rand_string + "." + random.choice(domains_in_def)
+    return my_str_1
+
+
+names = ["king", "miller", "kean"]
+domains = ["net", "com", "ua"]
+
+e_mail = create_email(names, domains)
+print(e_mail)
 
 
