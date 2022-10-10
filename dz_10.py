1fcc3c37eb29e2bd1c0aa426ea897a7157984a08
Yemelianov Vyacheslav <slavaemelanov80@gmail.com>
2022-10-10 15:56:46 +0300

    Dz_10 commit

--- dz_10.py
+++ dz_10.py
@@ -1,12 +1,14 @@
-#############################################################################
+# #############################################################################
 def domains_internet(filename):
+    my_1_txt = []
     with open(filename, "r") as name:
         text = name.read()
         domen_name = text.replace(".", "")
-    return domen_name
+        my_1_txt.append(domen_name)
+    return my_1_txt
 my_1_txt = domains_internet("domains.txt")
 print(my_1_txt)
-#############################################################################
+# #############################################################################
 def names (filename):
     with open(filename, "r") as name:
         text = '\t'.join([line.split()[1] for line in name.readlines()])
@@ -14,21 +16,17 @@ def names (filename):
 
 my_2_txt = names("names.txt")
 print(my_2_txt)
-#############################################################################
+# #############################################################################
 def authors (filename):
     with open("authors.txt", "r") as name:
-        data = [line.split("-") for line in name.readlines()]
+        data = [line.split("-")[0] for line in name.readlines()]
         new_list = []
-        new_list_2 = []
-        for index in range(len(data)):
-           new_str = data[index].pop(0)
-           new_list.append(new_str)
-        for count_1 in new_list:
+        for count_1 in data:
             dict = {}
             dict["data"] = count_1
             if count_1[0].isdigit():
-                new_list_2.append(dict)
-    return new_list_2
+                new_list.append(dict)
+    return new_list
 
 
 my_3_txt = authors("authors.txt")
